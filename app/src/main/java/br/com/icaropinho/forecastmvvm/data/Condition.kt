7eb55ac5c70767cf283fc9719cf2c768f7407e7d
Icaro Pinho Esmeraldo <icaro.pinho@solucaosistemas.net>
2019-01-15 09:51:18 -0300

    Generate model classes from json

---
+++ app/src/main/java/br/com/icaropinho/forecastmvvm/data/Condition.kt
@@ -0,0 +1,7 @@
+package br.com.icaropinho.forecastmvvm.data
+
+data class Condition(
+    val code: Int,
+    val icon: String,
+    val text: String
+)
---
+++ app/src/main/java/br/com/icaropinho/forecastmvvm/data/CurrentWeatherResponse.kt
@@ -0,0 +1,9 @@
+package br.com.icaropinho.forecastmvvm.data
+
+import com.google.gson.annotations.SerializedName
+
+data class CurrentWeatherResponse(
+    val location: Location,
+    @SerializedName("current")
+    val currentWeatherEntry: CurrentWeatherEntry
+)
